3b7620465dbde530464c5da168c9d59b64e4b992
aurterio6 <77596337+aurterio6@users.noreply.github.com>
2021-12-12 12:56:27 +0900

    extlnk

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,8 +13,11 @@ const RenderPage = () => (
         </p>
 
         <p>
-          一目瞭然ですが、easy-notion-blogを使わせてもらってます。
-          今は初めてコードに触るので、いつか...
+          一目瞭然ですが、
+          <ExtLink href="https://github.com/otoyo/easy-notion-blog">
+            easy-notion-blog
+          </ExtLink>
+          を使わせてもらってます。 今は初めてコードに触るので、いつか...
         </p>
       </div>
     </div>
