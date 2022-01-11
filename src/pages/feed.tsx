33b34dcebb6bb488e35da7123b05dbf990040d4d
aurterio6 <77596337+aurterio6@users.noreply.github.com>
2022-01-12 02:11:25 +0900

    add rss_feed

---
+++ src/pages/feed.tsx
@@ -0,0 +1,46 @@
+import { GetServerSidePropsContext } from 'next'
+import RSS from 'rss'
+import { getPosts, getAllPosts } from '../lib/notion/client'
+
+export const getServerSideProps = async ({
+  res,
+}: GetServerSidePropsContext) => {
+  const xml = await generateFeedXml() // フィードのXMLを生成する（後述）
+
+  res.statusCode = 200
+  res.setHeader('Cache-Control', 's-maxage=86400, stale-while-revalidate') // 24時間キャッシュする
+  res.setHeader('Content-Type', 'text/xml')
+  res.end(xml)
+
+  return {
+    props: {},
+  }
+}
+
+const Page = () => null
+export default Page
+
+async function generateFeedXml() {
+  const feed = new RSS({
+    title: 'MyLifelog',
+    description: 'ゆったりしてね',
+    site_url: 'https://www.pappardelle-nono.tk',
+    feed_url: '/feed',
+    language: 'ja',
+  })
+
+  // 例としてpostsを含めるイメージ
+  // このあたりの書き方はライブラリのドキュメントを参考にしてください
+  const posts = await getAllPosts()
+  posts?.forEach(post => {
+    feed.item({
+      title: post.Title,
+      description: post.Excerpt,
+      date: new Date(post.CreatedTime),
+      url: `https://www.pappardelle-nono.tk/${post.Slug}`,
+    })
+  })
+
+  // XML形式の文字列にする
+  return feed.xml()
+}
